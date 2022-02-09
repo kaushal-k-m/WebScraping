bf171781bc172db0abb941af468f1fcb26671ad4
kaushal Mahajan <62788748+kaushal-k-m@users.noreply.github.com>
2022-02-09 17:27:02 +0530

    Update midsouth.py

--- midsouth_scraping/midsouth_scraping/spiders/midsouth.py
+++ midsouth_scraping/midsouth_scraping/spiders/midsouth.py
@@ -5,26 +5,26 @@ class midsouthSpider(scrapy.Spider):
     start_urls=[
         'https://www.midsouthshooterssupply.com/dept/reloading/primers?currentpage=1'
     ]
-    custom_settings = {"FEEDS":{"results.json":{"format":"json"}}}
+    custom_settings = {"FEEDS":{"results.json":{"format":"json"}}} # creates json data stores in result
     def parse(self,response):
-        for supplies in response.css("div.product"):
-            price =supplies.css(".price span::text").get()
-            title = supplies.css(".catalog-item-name::text").get()
-            if supplies.css(".out-of-stock::text")[0].get() == 'Out of Stock':
+        for supplies in response.css("div.product"): # iterate through each product and collect one attribute at a time
+            price =supplies.css(".price span::text").get() #extracts rpice
+            title = supplies.css(".catalog-item-name::text").get() #extracts product title
+            if supplies.css(".out-of-stock::text")[0].get() == 'Out of Stock': #if the product is out of stock , stock is set to false
                 stock=False
             else:
                 stock=True
             
-            manufacturer =supplies.css(".catalog-item-brand::text").get()
+            manufacturer =supplies.css(".catalog-item-brand::text").get()# extracts manufacturer
 
-            yield {
+            yield { # data yeilded in json format
                 'price':price,
                 'title' : title,
                 'stock': stock,
                 'manufacturer' : manufacturer           
                 }
         
-        next_page = response.css(".pagination a:nth-child(5)::attr(href)").get()
+        next_page = response.css(".pagination a:nth-child(5)::attr(href)").get() # scraping for next page
         if next_page is not None:
             next_page=response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
